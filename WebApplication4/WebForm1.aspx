78f7c628b61a7d76c51194bf8c07291957776dd7
rayhanuddin56 <rayhanuddin55@live.com>
2016-02-27 01:23:43 +0600

    all file

---
+++ WebApplication4/WebForm1.aspx
@@ -0,0 +1,50 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication4.WebForm1" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="ID"></asp:Label>
+        <asp:TextBox ID="TextBoxID" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="NAME"></asp:Label>
+        <asp:TextBox ID="TextBoxName" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="EMAIL"></asp:Label>
+        <asp:TextBox ID="TextBoxEmail" runat="server"></asp:TextBox>
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="PHONE"></asp:Label>
+        <asp:TextBox ID="TextBoxPhone" runat="server"></asp:TextBox>
+        <br />
+        <asp:Button ID="ButtonSave" runat="server" onclick="ButtonSave_Click" 
+            Text="Save" />
+        <asp:Button ID="ButtonUpdateDB" runat="server" onclick="ButtonUpdateDB_Click" 
+            Text="UpdateDB" />
+        <asp:Button ID="Undo" runat="server" onclick="Undo_Click" Text="Undo" />
+        <br />
+        <br />
+        <asp:Label ID="LabelMessage" runat="server"></asp:Label>
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+            DataKeyNames="StudentId" onrowcancelingedit="GridView1_RowCancelingEdit" 
+            onrowdeleting="GridView1_RowDeleting" onrowediting="GridView1_RowEditing" 
+            onrowupdating="GridView1_RowUpdating">
+            <Columns>
+                <asp:BoundField DataField="StudentId" HeaderText="AIUB ID" ReadOnly="True" />
+                <asp:BoundField DataField="StudentName" HeaderText="NAME" />
+                <asp:BoundField DataField="Email" HeaderText="EMAIL" />
+                <asp:BoundField DataField="Phone" HeaderText="PHONE" />
+                <asp:CommandField HeaderText="OPTIONS" ShowDeleteButton="True" 
+                    ShowEditButton="True" />
+            </Columns>
+        </asp:GridView>
+        <br />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication4/WebForm1.aspx.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WebApplication4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        private void LoadFromDatabase()
+        {
+            string sql = "SELECT * FROM Students";
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(connStr);
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Students");
+
+            ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["StudentId"] };
+
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            Cache.Insert("ADAPTER", adapter, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+
+        }
+
+        private void LoadGridViewData()
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            GridView1.DataSource = ds.Tables["Students"];
+            GridView1.DataBind();
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            this.LoadGridViewData();
+        }
+
+        protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+            GridView1.EditIndex = e.NewEditIndex;
+            this.LoadGridViewData();
+
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["StudentId"]);
+
+            dr.Delete();
+
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            this.LoadGridViewData();
+        }
+
+        protected void GridView1_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+            GridView1.EditIndex = -1;
+            this.LoadGridViewData();
+        }
+
+        protected void GridView1_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            DataRow dr = ds.Tables["Students"].Rows.Find(e.Keys["StudentId"]);
+            dr["StudentName"] = e.NewValues["StudentName"];
+            dr["Email"] = e.NewValues["Email"];
+            dr["Phone"] = e.NewValues["Phone"];
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            GridView1.EditIndex = -1;
+            this.LoadGridViewData();
+
+        }
+
+        protected void ButtonSave_Click(object sender, EventArgs e)
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            DataRow dr = ds.Tables["Students"].NewRow();
+            dr["StudentId"] = TextBoxID.Text;
+            dr["StudentName"] = TextBoxName.Text;
+            dr["Email"] = TextBoxEmail.Text;
+            dr["Phone"] = TextBoxPhone.Text;
+            ds.Tables["Students"].Rows.Add(dr);
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            this.LoadGridViewData();
+            LabelMessage.Text = "Insert successfull";
+        }
+
+        protected void Undo_Click(object sender, EventArgs e)
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            if (ds.HasChanges())
+            {
+                ds.Tables["Students"].RejectChanges();
+                Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+                this.LoadGridViewData();
+                LabelMessage.Text = "Undo Complete";
+            }
+            else
+            {
+                LabelMessage.Text = "Nothing to be undone";
+            }
+            
+           
+        }
+
+        protected void ButtonUpdateDB_Click(object sender, EventArgs e)
+        {
+            if (Cache["DATASET"] == null)
+            {
+                this.LoadFromDatabase();
+            }
+
+            DataSet ds = (DataSet)Cache["DATASET"];
+            SqlDataAdapter adapter = (SqlDataAdapter)Cache["ADAPTER"];
+            adapter.Update(ds.Tables["Students"]);
+            ds.Tables["Students"].AcceptChanges();
+            LabelMessage.Text = "Saved to database";
+        }
+    }
+}
