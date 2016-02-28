a30538637aa58ae74cc8db1dff783b34a60155b9
Rayhan Uddin <rayhanuddin55@gmail.com>
2016-02-28 18:12:48 +0600

    load from database add on 3

--- WebApplication4/WebForm3.aspx.cs
+++ WebApplication4/WebForm3.aspx.cs
@@ -9,6 +9,23 @@ namespace WebApplication4
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+        private void LoadFromDatabase()  // nessery function
+        {
+            string sql = "SELECT * FROM Students";
+            string connStr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(connStr);
+            SqlDataAdapter adapter = new SqlDataAdapter(sql, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "Students");
+
+            ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["StudentId"] };
+
+            Cache.Insert("DATASET", ds, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+            Cache.Insert("ADAPTER", adapter, null, DateTime.Now.AddHours(1), System.Web.Caching.Cache.NoSlidingExpiration);
+
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
 
